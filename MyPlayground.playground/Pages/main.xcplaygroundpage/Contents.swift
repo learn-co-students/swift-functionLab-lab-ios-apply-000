cfe6f990e9a9d9bed938c591090b447217f95cb0
sejdemi <sejan.miah@gmail.com>
2016-12-13 15:29:53 -0500

    Functions Lab

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -11,8 +11,10 @@
 /*: question1
  ### 1. Write a function called `helloWorld()` that prints "Hello, world!" to the console. Then call it to see your string printed to the playground console.
  */
-// write your code here
-
+func helloWorld() {
+    print("Hello, world!")
+}
+helloWorld()
 
 
 
@@ -22,20 +24,37 @@
 // write your code here
 
 
+func sayMyName() {
+    let myName = "Sejan"
+    print(myName)
+}
 
-
+sayMyName()
 /*: question3
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
-// write your code here
+func takesName(_ name: String) {
+//    let name = "Sejan Miah"
+    print("Hey \(name). How's it going?")
+    
+}
 
+takesName("Sejan Miah")
+takesName("John Snow")
 
 
 
 /*: question4
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
-// write your code here
+var otherName = "Sarah"
+
+takesName(otherName)
+
+otherName = "Sharmila"
+
+takesName(otherName)
+
 
 
 
@@ -43,10 +62,16 @@
 /*: question5
  ### 5. Write your own function in which you declare a _variable_ (of any type) inside the function's body. Print out this variable to the console from within your function. After you print the variable once, assign it to a new variable on the next line. Print it again (after the line on which you assign it to a new value). Call your function several times. What do you expect to see printed to the playground's console each time you call this function?
  */
-// write your code here
+func otherGreeting() {
+    var name = "Sejan"
+    print(name)
+    
+    var name2 = "Sarah"
+    print(name2)
+}
 
 
-
+otherGreeting()
 
 /*:
 Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions. */
